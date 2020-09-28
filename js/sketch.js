51fd4b0c9d651c11fe91e59c0ca7bc01ec1625a7
Piene Luijpers <71266227+pieneluijpers@users.noreply.github.com>
2020-09-28 14:03:19 +0200

    Add files via upload

---
+++ js/sketch.js
@@ -0,0 +1,40 @@
+function setup() {
+  createCanvas(500,500);
+  background(255);
+  // put setup code here
+
+  strokeWeight(6)
+  fill(255)
+  rect(370,245,125,125)
+  //wit
+
+  strokeWeight(6)
+  fill(255,0,0)
+  rect(250,5,250,250)
+//rood vierkant
+
+
+
+  fill(255)
+  rect(00,10,250,125)
+//boven
+strokeWeight(6)
+  fill(255)
+  rect(0,130,250,125)
+  //beneden
+
+  rect(250,370,125,125)
+
+  fill(0,118,244)
+  rect(0,255,250,240)
+  //blauw
+
+  fill(255,213,0)
+  rect(370,370,125,125)
+  //geel
+}
+
+
+function draw() {
+  // put drawing code here
+}
